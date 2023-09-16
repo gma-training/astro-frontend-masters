f4ed2765f5201ade77c3d4414727b48f6201898a
Graham Ashton <graham@effectif.com>
2023-09-16 18:37:14 +0100

    Add RSS feed listing all blog posts
    Note the double file extension on the RSS feed's file. It indicates to
    Astro that we'll be sending back a content type other than HTML; we
    could use the same trick for JSON if we wanted to.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import solidJs from '@astrojs/solid-js';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://astro-frontend-masters-gma.netlify.app',
 	integrations: [
 		react({
 			include: ['**/add-to-cart*'],
